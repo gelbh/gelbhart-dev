efb0db980e50689c8d0fb880ab4ff83f239dac70
gelbh <gelbharttomer@gmail.com>
2024-11-21 22:03:37 +0100

    Fix asset configuration for Render deployment

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
-//= link_tree ../builds
-//= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link_directory ../stylesheets .css
+//= link_directory ../builds
+//= link application.css
+//= link application.js
