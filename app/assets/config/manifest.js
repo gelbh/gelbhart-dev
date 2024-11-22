8b4055ab796c5a0848fbde62d0491ceac4aa2a1a
gelbh <gelbharttomer@gmail.com>
2024-11-22 02:33:30 +0100

    fixed manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,13 @@
-//= link_tree ../images
 //= link application.js
 //= link application.scss
+//= link controllers/application.js
+
+//= link_tree ../images
+
+//= link_tree ../../javascript/theme .js
+//= link_tree ../../javascript/theme/components js
+//= link_tree ../../javascript/controllers .jsx
+
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
 //= link_directory ../stylesheets/theme .scss
-//= link ../../javascript/controllers/application.js
-//= link_directory ../../javascript/controllers .js
--- app/controllers/health_controller.rb
+++
@@ -1,7 +0,0 @@
-class HealthController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def show
-    render plain: "ok", status: :ok
-  end
-end
