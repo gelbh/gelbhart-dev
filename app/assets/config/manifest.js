f2585f9a10c7392dcaa6c92df78d2231cf8197cd
gelbh <gelbharttomer@gmail.com>
2024-11-22 02:24:36 +0100

    updated manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
-//= link_tree controllers .js
 //= link application.js
 //= link application.scss
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/theme .scss
-//= link controllers/application.js
+//= link ../../javascript/controllers/application.js
+//= link_directory ../../javascript/controllers .js
