63bbfabc2099f7202bf28b363ba8615b55d8f5df
gelbh <gelbharttomer@gmail.com>
2024-11-22 18:32:51 +0100

    added error templates

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,11 +3,15 @@ Rails.application.config.assets.version = "1.0"
 
 # Add asset paths
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/scss")
+
+Rails.application.config.assets.paths << Rails.root.join("app/javascript")
 Rails.application.config.assets.paths << Rails.root.join("app/assets/fonts")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/json")
+
 Rails.application.config.assets.paths << Rails.root.join("vendor/javascripts")
 Rails.application.config.assets.paths << Rails.root.join("vendor/assets/stylesheets")
-Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
-Rails.application.config.assets.paths << Rails.root.join("app/javascript")
 
 # Precompile additional assets
 Rails.application.config.assets.precompile += %w[
@@ -22,6 +26,3 @@ Rails.application.config.assets.precompile += %w[
 # Allow non-digest files for fonts
 Rails.application.config.assets.compile = true
 Rails.application.config.assets.digest = true
-
-# Specify node_modules as additional sprockets load path
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/scss")
