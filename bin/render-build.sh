277c8ec0c992b13ec7f696f326341c32f58ceff6
gelbh <gelbharttomer@gmail.com>
2025-01-06 20:49:49 -0500

    feat: streamline asset compilation and remove sitemap generator cron job

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,23 +5,9 @@ set -o errexit
 # Install dependencies
 bundle install
 
-# Install npm packages
-npm install bootstrap @popperjs/core
-
-# Create necessary asset directories
-mkdir -p app/assets/builds
-mkdir -p app/assets/images
-mkdir -p app/assets/stylesheets/theme
-mkdir -p vendor/assets/stylesheets
-
-# Copy Bootstrap SCSS files to vendor
-cp -r node_modules/bootstrap/scss vendor/assets/stylesheets/bootstrap
-
-# Clean assets
+# Compile assets
+bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
-# Precompile assets with proper environment
-RAILS_ENV=production SECRET_KEY_BASE=dummy bundle exec rake assets:precompile
-
-# Run database migrations
+# Set up database
 bundle exec rake db:migrate
