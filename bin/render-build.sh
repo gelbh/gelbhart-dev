200ce7386e95a7ed9489428e28e8feb81ece1374
gelbh <gelbharttomer@gmail.com>
2025-10-01 17:04:45 +0100

    fix: add npm install to build script for Bootstrap dependencies

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install Ruby dependencies
 bundle install
 
+# Install Node dependencies
+npm install
+
 # Install Python dependencies for video captioner
 pip install -r requirements.txt
 
