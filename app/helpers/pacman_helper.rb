3e3fa733a41de249233787681864b76ca79b9272
gelbh <gelbharttomer@gmail.com>
2025-10-02 14:12:13 +0100

    Fix asset manifest key mismatch causing sprite disappearance
    The Rails helper was using relative paths as keys (e.g., 'pacman/pacman_open_more.png')
    while JavaScript looked them up with full paths (e.g., 'pacman-game/pacman/pacman_open_more.png').
    
    This mismatch caused sprites to randomly fail to load in production, resulting in
    ERR_HTTP2_PROTOCOL_ERROR when the fallback paths didn't work.
    
    Updated all manifest keys to match JavaScript lookup patterns with 'pacman-game/' prefix.

--- app/helpers/pacman_helper.rb
+++ app/helpers/pacman_helper.rb
@@ -1,62 +1,62 @@
 module PacmanHelper
   def pacman_asset_manifest
     {
-      # Pac-Man sprites
-      'pacman/pacman_open_more.png' => asset_path('pacman-game/pacman/pacman_open_more.png'),
-      'pacman/pacman_open_less.png' => asset_path('pacman-game/pacman/pacman_open_less.png'),
-      'pacman/pacman_closed.png' => asset_path('pacman-game/pacman/pacman_closed.png'),
+      # Pac-Man sprites - keys match JavaScript lookups with 'pacman-game/' prefix
+      'pacman-game/pacman/pacman_open_more.png' => asset_path('pacman-game/pacman/pacman_open_more.png'),
+      'pacman-game/pacman/pacman_open_less.png' => asset_path('pacman-game/pacman/pacman_open_less.png'),
+      'pacman-game/pacman/pacman_closed.png' => asset_path('pacman-game/pacman/pacman_closed.png'),
 
       # Audio files
-      'sounds/pacman_beginning.wav' => asset_path('pacman-game/sounds/pacman_beginning.wav'),
-      'sounds/pacman_chomp.wav' => asset_path('pacman-game/sounds/pacman_chomp.wav'),
-      'sounds/pacman_death.wav' => asset_path('pacman-game/sounds/pacman_death.wav'),
-      'sounds/pacman_eatfruit.wav' => asset_path('pacman-game/sounds/pacman_eatfruit.wav'),
-      'sounds/pacman_eatghost.wav' => asset_path('pacman-game/sounds/pacman_eatghost.wav'),
-      'sounds/pacman_extrapac.wav' => asset_path('pacman-game/sounds/pacman_extrapac.wav'),
-      'sounds/pacman_intermission.wav' => asset_path('pacman-game/sounds/pacman_intermission.wav'),
-      
+      'pacman-game/sounds/pacman_beginning.wav' => asset_path('pacman-game/sounds/pacman_beginning.wav'),
+      'pacman-game/sounds/pacman_chomp.wav' => asset_path('pacman-game/sounds/pacman_chomp.wav'),
+      'pacman-game/sounds/pacman_death.wav' => asset_path('pacman-game/sounds/pacman_death.wav'),
+      'pacman-game/sounds/pacman_eatfruit.wav' => asset_path('pacman-game/sounds/pacman_eatfruit.wav'),
+      'pacman-game/sounds/pacman_eatghost.wav' => asset_path('pacman-game/sounds/pacman_eatghost.wav'),
+      'pacman-game/sounds/pacman_extrapac.wav' => asset_path('pacman-game/sounds/pacman_extrapac.wav'),
+      'pacman-game/sounds/pacman_intermission.wav' => asset_path('pacman-game/sounds/pacman_intermission.wav'),
+
       # Blinky (red ghost) sprites
-      'ghosts/blinky-right-1.png' => asset_path('pacman-game/ghosts/blinky-right-1.png'),
-      'ghosts/blinky-right-2.png' => asset_path('pacman-game/ghosts/blinky-right-2.png'),
-      'ghosts/blinky-down-1.png' => asset_path('pacman-game/ghosts/blinky-down-1.png'),
-      'ghosts/blinky-down-2.png' => asset_path('pacman-game/ghosts/blinky-down-2.png'),
-      'ghosts/blinky-up-1.png' => asset_path('pacman-game/ghosts/blinky-up-1.png'),
-      'ghosts/blinky-up-2.png' => asset_path('pacman-game/ghosts/blinky-up-2.png'),
-      
+      'pacman-game/ghosts/blinky-right-1.png' => asset_path('pacman-game/ghosts/blinky-right-1.png'),
+      'pacman-game/ghosts/blinky-right-2.png' => asset_path('pacman-game/ghosts/blinky-right-2.png'),
+      'pacman-game/ghosts/blinky-down-1.png' => asset_path('pacman-game/ghosts/blinky-down-1.png'),
+      'pacman-game/ghosts/blinky-down-2.png' => asset_path('pacman-game/ghosts/blinky-down-2.png'),
+      'pacman-game/ghosts/blinky-up-1.png' => asset_path('pacman-game/ghosts/blinky-up-1.png'),
+      'pacman-game/ghosts/blinky-up-2.png' => asset_path('pacman-game/ghosts/blinky-up-2.png'),
+
       # Pinky (pink ghost) sprites
-      'ghosts/pinky-right-1.png' => asset_path('pacman-game/ghosts/pinky-right-1.png'),
-      'ghosts/pinky-right-2.png' => asset_path('pacman-game/ghosts/pinky-right-2.png'),
-      'ghosts/pinky-down-1.png' => asset_path('pacman-game/ghosts/pinky-down-1.png'),
-      'ghosts/pinky-down-2.png' => asset_path('pacman-game/ghosts/pinky-down-2.png'),
-      'ghosts/pinky-up-1.png' => asset_path('pacman-game/ghosts/pinky-up-1.png'),
-      'ghosts/pinky-up-2.png' => asset_path('pacman-game/ghosts/pinky-up-2.png'),
-      
+      'pacman-game/ghosts/pinky-right-1.png' => asset_path('pacman-game/ghosts/pinky-right-1.png'),
+      'pacman-game/ghosts/pinky-right-2.png' => asset_path('pacman-game/ghosts/pinky-right-2.png'),
+      'pacman-game/ghosts/pinky-down-1.png' => asset_path('pacman-game/ghosts/pinky-down-1.png'),
+      'pacman-game/ghosts/pinky-down-2.png' => asset_path('pacman-game/ghosts/pinky-down-2.png'),
+      'pacman-game/ghosts/pinky-up-1.png' => asset_path('pacman-game/ghosts/pinky-up-1.png'),
+      'pacman-game/ghosts/pinky-up-2.png' => asset_path('pacman-game/ghosts/pinky-up-2.png'),
+
       # Inky (cyan ghost) sprites
-      'ghosts/inky-right-1.png' => asset_path('pacman-game/ghosts/inky-right-1.png'),
-      'ghosts/inky-right-2.png' => asset_path('pacman-game/ghosts/inky-right-2.png'),
-      'ghosts/inky-down-1.png' => asset_path('pacman-game/ghosts/inky-down-1.png'),
-      'ghosts/inky-down-2.png' => asset_path('pacman-game/ghosts/inky-down-2.png'),
-      'ghosts/inky-up-1.png' => asset_path('pacman-game/ghosts/inky-up-1.png'),
-      'ghosts/inky-up-2.png' => asset_path('pacman-game/ghosts/inky-up-2.png'),
-      
+      'pacman-game/ghosts/inky-right-1.png' => asset_path('pacman-game/ghosts/inky-right-1.png'),
+      'pacman-game/ghosts/inky-right-2.png' => asset_path('pacman-game/ghosts/inky-right-2.png'),
+      'pacman-game/ghosts/inky-down-1.png' => asset_path('pacman-game/ghosts/inky-down-1.png'),
+      'pacman-game/ghosts/inky-down-2.png' => asset_path('pacman-game/ghosts/inky-down-2.png'),
+      'pacman-game/ghosts/inky-up-1.png' => asset_path('pacman-game/ghosts/inky-up-1.png'),
+      'pacman-game/ghosts/inky-up-2.png' => asset_path('pacman-game/ghosts/inky-up-2.png'),
+
       # Clyde (orange ghost) sprites
-      'ghosts/clyde-right-1.png' => asset_path('pacman-game/ghosts/clyde-right-1.png'),
-      'ghosts/clyde-right-2.png' => asset_path('pacman-game/ghosts/clyde-right-2.png'),
-      'ghosts/clyde-down-1.png' => asset_path('pacman-game/ghosts/clyde-down-1.png'),
-      'ghosts/clyde-down-2.png' => asset_path('pacman-game/ghosts/clyde-down-2.png'),
-      'ghosts/clyde-up-1.png' => asset_path('pacman-game/ghosts/clyde-up-1.png'),
-      'ghosts/clyde-up-2.png' => asset_path('pacman-game/ghosts/clyde-up-2.png'),
-      
+      'pacman-game/ghosts/clyde-right-1.png' => asset_path('pacman-game/ghosts/clyde-right-1.png'),
+      'pacman-game/ghosts/clyde-right-2.png' => asset_path('pacman-game/ghosts/clyde-right-2.png'),
+      'pacman-game/ghosts/clyde-down-1.png' => asset_path('pacman-game/ghosts/clyde-down-1.png'),
+      'pacman-game/ghosts/clyde-down-2.png' => asset_path('pacman-game/ghosts/clyde-down-2.png'),
+      'pacman-game/ghosts/clyde-up-1.png' => asset_path('pacman-game/ghosts/clyde-up-1.png'),
+      'pacman-game/ghosts/clyde-up-2.png' => asset_path('pacman-game/ghosts/clyde-up-2.png'),
+
       # Frightened ghost sprites
-      'ghosts/frightened-blue-1.png' => asset_path('pacman-game/ghosts/frightened-blue-1.png'),
-      'ghosts/frightened-blue-2.png' => asset_path('pacman-game/ghosts/frightened-blue-2.png'),
-      'ghosts/frightened-white-1.png' => asset_path('pacman-game/ghosts/frightened-white-1.png'),
-      'ghosts/frightened-white-2.png' => asset_path('pacman-game/ghosts/frightened-white-2.png'),
-      
+      'pacman-game/ghosts/frightened-blue-1.png' => asset_path('pacman-game/ghosts/frightened-blue-1.png'),
+      'pacman-game/ghosts/frightened-blue-2.png' => asset_path('pacman-game/ghosts/frightened-blue-2.png'),
+      'pacman-game/ghosts/frightened-white-1.png' => asset_path('pacman-game/ghosts/frightened-white-1.png'),
+      'pacman-game/ghosts/frightened-white-2.png' => asset_path('pacman-game/ghosts/frightened-white-2.png'),
+
       # Ghost eyes sprites
-      'ghosts/eyes-right.png' => asset_path('pacman-game/ghosts/eyes-right.png'),
-      'ghosts/eyes-down.png' => asset_path('pacman-game/ghosts/eyes-down.png'),
-      'ghosts/eyes-up.png' => asset_path('pacman-game/ghosts/eyes-up.png')
+      'pacman-game/ghosts/eyes-right.png' => asset_path('pacman-game/ghosts/eyes-right.png'),
+      'pacman-game/ghosts/eyes-down.png' => asset_path('pacman-game/ghosts/eyes-down.png'),
+      'pacman-game/ghosts/eyes-up.png' => asset_path('pacman-game/ghosts/eyes-up.png')
     }
   end
 end
