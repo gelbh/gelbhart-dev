c67b9f82c518d2eeab4e3c66646b6ba6d32dea8a
gelbh <gelbharttomer@gmail.com>
2025-11-18 15:03:28 +0000

    fix(stimulus): override load method and preload controllers

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,7 @@
 import { Application } from "@hotwired/stimulus";
 
-// Create application instance but don't start it yet
-// It will be started in index.js after all controllers are registered
-const application = new Application();
+const application = Application.start();
 
-// Configure Stimulus development experience
 application.debug = false;
 
 window.Stimulus = application;
