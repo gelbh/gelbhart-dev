b0e22fa6e55f950269a3094ae02f51eaab387497
gelbh <gelbharttomer@gmail.com>
2025-11-18 14:36:21 +0000

    fix(stimulus): disable lazy-loading to prevent 404 errors in production

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,10 @@ const application = Application.start();
 
 // Configure Stimulus development experience
 application.debug = false;
+
+// Disable automatic lazy-loading - all controllers are eagerly loaded in index.js
+application.loadDefinitionsFromContext = false;
+
 window.Stimulus = application;
 
 export { application };
