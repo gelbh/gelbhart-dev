4016707c7ccf4ca55353cf06db2c05e45822dac7
gelbh <gelbharttomer@gmail.com>
2025-11-18 19:05:56 +0000

    fix: use importmap paths instead of relative imports in controllers/index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,17 +1,17 @@
-import { application } from "./application";
+import { application } from "controllers/application";
 
 // Explicitly import and register all controllers BEFORE starting Stimulus
 // This ensures all controllers are registered before Stimulus scans the DOM
 // and prevents lazy-loading attempts that cause 404 errors
 
-import AnalyticsStatsController from "./analytics_stats_controller";
-import CodeTyperController from "./code_typer_controller";
-import CounterController from "./counter_controller";
-import PacmanGameController from "./pacman_game_controller";
-import PacmanPreviewController from "./pacman-preview_controller";
-import ScrollAnimationController from "./scroll_animation_controller";
-import ScrollToTopController from "./scroll_to_top_controller";
-import ThemeController from "./theme_controller";
+import AnalyticsStatsController from "controllers/analytics_stats_controller";
+import CodeTyperController from "controllers/code_typer_controller";
+import CounterController from "controllers/counter_controller";
+import PacmanGameController from "controllers/pacman_game_controller";
+import PacmanPreviewController from "controllers/pacman-preview_controller";
+import ScrollAnimationController from "controllers/scroll_animation_controller";
+import ScrollToTopController from "controllers/scroll_to_top_controller";
+import ThemeController from "controllers/theme_controller";
 
 // Register all controllers with their kebab-case names
 application.register("analytics-stats", AnalyticsStatsController);
