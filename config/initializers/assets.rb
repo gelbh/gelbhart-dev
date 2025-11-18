247b8e0f59a42043cbfa42d017f3e9dedb6a9023
gelbh <gelbharttomer@gmail.com>
2025-11-18 13:54:33 +0000

    refactor(assets): remove custom asset pipeline configuration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,47 +1,7 @@
-# Asset version for cache busting
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-# Configure asset pipeline behavior
-Rails.application.config.assets.digest = true
-
-# Add asset paths grouped by type
-Rails.application.config.assets.paths += [
-  # Application assets
-  Rails.root.join("app/javascript"),
-  Rails.root.join("app/assets/fonts"),
-  Rails.root.join("app/assets/builds"),
-  Rails.root.join("app/assets/json"),
-
-  # NPM package assets
-  Rails.root.join("node_modules"),
-  Rails.root.join("node_modules/bootstrap/scss")
-]
-
-# Precompile rules grouped by type
-Rails.application.config.assets.precompile += [
-  # Font files
-  ".eot",
-  ".woff",
-  ".woff2",
-  ".ttf",
-
-  # Image files
-  ".svg",
-  ".png",
-  ".jpg",
-  ".gif",
-
-  # Audio files
-  ".wav",
-  ".mp3",
-  ".ogg",
-
-  # JavaScript files
-  "application.js",
-  "bootstrap.min.js",
-  "theme/*.js",
-
-  # CSS files
-  "*.css",
-  "application.bootstrap.css"
-].flatten
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
