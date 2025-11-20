ca728e78672ea2b01122723ae3382e608eb52ca3
gelbh <gelbharttomer@gmail.com>
2025-11-20 18:00:14 +0000

    fix(puma): configure single-mode for Render starter plan

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 # Puma can serve each request in a thread from an internal thread pool.
-workers ENV.fetch("WEB_CONCURRENCY") { 1 }
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 2 }
+worker_count = ENV.fetch("WEB_CONCURRENCY") { 0 }.to_i
+workers worker_count if worker_count > 0
+
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
@@ -16,15 +18,18 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-preload_app!
+# Only use cluster mode features when in cluster mode (multiple workers)
+if worker_count > 0
+  preload_app!
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  before_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 
-  # Log memory usage on worker boot for monitoring
-  if defined?(Rails) && Rails.logger
-    memory_mb = get_memory_usage_mb
-    Rails.logger.info "Puma worker booted - Memory: #{memory_mb} MB"
+    # Log memory usage on worker boot for monitoring
+    if defined?(Rails) && Rails.logger
+      memory_mb = get_memory_usage_mb
+      Rails.logger.info "Puma worker booted - Memory: #{memory_mb} MB"
+    end
   end
 end
 
