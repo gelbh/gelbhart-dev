93c616b07cf9e7c6fa025c2c01a32c86aff5ce07
gelbh <gelbharttomer@gmail.com>
2025-11-30 01:13:04 +0000

    feat(seo): add structured data and enhanced meta tags

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,18 +8,20 @@ SitemapGenerator::Sitemap.sitemaps_path = ""
 SitemapGenerator::Sitemap.compress = false
 
 SitemapGenerator::Sitemap.create do
+  current_time = Time.now
+
   # Home page
-  add "/", changefreq: "weekly", priority: 1.0
+  add "/", changefreq: "weekly", priority: 1.0, lastmod: current_time
 
   # Hevy Tracker pages
-  add "/hevy-tracker", changefreq: "monthly", priority: 0.8
-  add "/hevy-tracker/privacy", changefreq: "yearly", priority: 0.5
-  add "/hevy-tracker/terms", changefreq: "yearly", priority: 0.5
+  add "/hevy-tracker", changefreq: "monthly", priority: 0.8, lastmod: current_time
+  add "/hevy-tracker/privacy", changefreq: "yearly", priority: 0.5, lastmod: current_time
+  add "/hevy-tracker/terms", changefreq: "yearly", priority: 0.5, lastmod: current_time
 
   # Contact page
-  add "/contact", changefreq: "monthly", priority: 0.8
+  add "/contact", changefreq: "monthly", priority: 0.8, lastmod: current_time
 
   # Other project pages
-  add "/video-captioner", changefreq: "monthly", priority: 0.7
-  add "/nasa-exoplanet-explorer", changefreq: "monthly", priority: 0.7
+  add "/video-captioner", changefreq: "monthly", priority: 0.7, lastmod: current_time
+  add "/nasa-exoplanet-explorer", changefreq: "monthly", priority: 0.7, lastmod: current_time
 end
