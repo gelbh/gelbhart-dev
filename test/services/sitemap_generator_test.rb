0dde18f46e4b5e452fa293b75b9b7d775a5ae028
gelbh <gelbharttomer@gmail.com>
2025-11-30 00:24:16 +0000

    refactor(sitemap): remove wait logic and test synchronization

--- test/services/sitemap_generator_test.rb
+++ test/services/sitemap_generator_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class SitemapGeneratorTest < ActiveSupport::TestCase
-  include SitemapTestSynchronization
+  # Disable parallelization for sitemap tests to avoid file system race conditions
+  # These tests generate files in the public directory which can conflict across processes
+  parallelize(workers: 1)
 
   setup do
     @generator = SitemapService.new
@@ -10,62 +12,52 @@ class SitemapGeneratorTest < ActiveSupport::TestCase
   end
 
   teardown do
-    # Clean up generated files with mutex protection
-    SitemapTestSynchronization::SITEMAP_MUTEX.synchronize do
-      @sitemap_path.unlink if @sitemap_path.exist?
-      @sitemap_gz_path.unlink if @sitemap_gz_path.exist?
-    end
+    # Clean up generated files
+    @sitemap_path.unlink if @sitemap_path.exist?
+    @sitemap_gz_path.unlink if @sitemap_gz_path.exist?
   rescue StandardError => e
     # Ignore cleanup errors
     Rails.logger.debug "Sitemap cleanup error: #{e.message}"
   end
 
   test "generate creates sitemap.xml and sitemap.xml.gz" do
-    SitemapTestSynchronization::SITEMAP_MUTEX.synchronize do
-      @generator.generate
+    @generator.generate
 
-      assert @sitemap_path.exist?, "sitemap.xml should be created"
-      assert @sitemap_gz_path.exist?, "sitemap.xml.gz should be created"
-    end
+    assert @sitemap_path.exist?, "sitemap.xml should be created"
+    assert @sitemap_gz_path.exist?, "sitemap.xml.gz should be created"
   end
 
   test "generated XML includes all routes" do
-    SitemapTestSynchronization::SITEMAP_MUTEX.synchronize do
-      @generator.generate
+    @generator.generate
 
-      xml_content = File.read(@sitemap_path)
-      assert_includes xml_content, "https://gelbhart.dev/"
-      assert_includes xml_content, "https://gelbhart.dev/hevy-tracker"
-      assert_includes xml_content, "https://gelbhart.dev/contact"
-      assert_includes xml_content, "https://gelbhart.dev/video-captioner"
-      assert_includes xml_content, "https://gelbhart.dev/nasa-exoplanet-explorer"
-    end
+    xml_content = File.read(@sitemap_path)
+    assert_includes xml_content, "https://gelbhart.dev/"
+    assert_includes xml_content, "https://gelbhart.dev/hevy-tracker"
+    assert_includes xml_content, "https://gelbhart.dev/contact"
+    assert_includes xml_content, "https://gelbhart.dev/video-captioner"
+    assert_includes xml_content, "https://gelbhart.dev/nasa-exoplanet-explorer"
   end
 
   test "generated XML has correct structure" do
-    SitemapTestSynchronization::SITEMAP_MUTEX.synchronize do
-      @generator.generate
+    @generator.generate
 
-      xml_content = File.read(@sitemap_path)
-      assert_includes xml_content, 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'
-      assert_includes xml_content, "<urlset"
-      assert_includes xml_content, "<url>"
-      assert_includes xml_content, "<loc>"
-      assert_includes xml_content, "<changefreq>"
-      assert_includes xml_content, "<priority>"
-      assert_includes xml_content, "<lastmod>"
-    end
+    xml_content = File.read(@sitemap_path)
+    assert_includes xml_content, 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'
+    assert_includes xml_content, "<urlset"
+    assert_includes xml_content, "<url>"
+    assert_includes xml_content, "<loc>"
+    assert_includes xml_content, "<changefreq>"
+    assert_includes xml_content, "<priority>"
+    assert_includes xml_content, "<lastmod>"
   end
 
   test "generated gzip file is compressed" do
-    SitemapTestSynchronization::SITEMAP_MUTEX.synchronize do
-      @generator.generate
+    @generator.generate
 
-      assert @sitemap_gz_path.exist?
-      # Gzip file should be smaller than uncompressed (or at least exist)
-      assert @sitemap_gz_path.size > 0
-      # Verify it's actually compressed (smaller than uncompressed)
-      assert @sitemap_gz_path.size < @sitemap_path.size, "Gzip file should be smaller than uncompressed XML"
-    end
+    assert @sitemap_gz_path.exist?
+    # Gzip file should be smaller than uncompressed (or at least exist)
+    assert @sitemap_gz_path.size > 0
+    # Verify it's actually compressed (smaller than uncompressed)
+    assert @sitemap_gz_path.size < @sitemap_path.size, "Gzip file should be smaller than uncompressed XML"
   end
 end
