4d21958f4dd99f2f03ed5d69a9b383c49e8e74c4
gelbh <gelbharttomer@gmail.com>
2026-01-06 21:52:49 +0100

    fix(db): comment out Supabase-managed functions to prevent permission errors

--- db/structure.sql
+++ db/structure.sql
@@ -98,83 +98,83 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UU
 -- Name: grant_pg_cron_access(); Type: FUNCTION; Schema: extensions; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION extensions.grant_pg_cron_access() RETURNS event_trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
-  IF EXISTS (
-    SELECT
-    FROM pg_event_trigger_ddl_commands() AS ev
-    JOIN pg_extension AS ext
-    ON ev.objid = ext.oid
-    WHERE ext.extname = 'pg_cron'
-  )
-  THEN
-    grant usage on schema cron to postgres with grant option;
-
-    alter default privileges in schema cron grant all on tables to postgres with grant option;
-    alter default privileges in schema cron grant all on functions to postgres with grant option;
-    alter default privileges in schema cron grant all on sequences to postgres with grant option;
-
-    alter default privileges for user supabase_admin in schema cron grant all
-        on sequences to postgres with grant option;
-    alter default privileges for user supabase_admin in schema cron grant all
-        on tables to postgres with grant option;
-    alter default privileges for user supabase_admin in schema cron grant all
-        on functions to postgres with grant option;
-
-    grant all privileges on all tables in schema cron to postgres with grant option;
-    revoke all on table cron.job from postgres;
-    grant select on table cron.job to postgres with grant option;
-  END IF;
-END;
-$$;
+-- CREATE FUNCTION extensions.grant_pg_cron_access() RETURNS event_trigger -- Supabase-managed function, commented out
+--     LANGUAGE plpgsql
+--     AS $$
+-- BEGIN
+--   IF EXISTS (
+--     SELECT
+--     FROM pg_event_trigger_ddl_commands() AS ev
+--     JOIN pg_extension AS ext
+--     ON ev.objid = ext.oid
+--     WHERE ext.extname = 'pg_cron'
+--   )
+--   THEN
+--     grant usage on schema cron to postgres with grant option;
+-- 
+--     alter default privileges in schema cron grant all on tables to postgres with grant option;
+--     alter default privileges in schema cron grant all on functions to postgres with grant option;
+--     alter default privileges in schema cron grant all on sequences to postgres with grant option;
+-- 
+--     alter default privileges for user supabase_admin in schema cron grant all
+--         on sequences to postgres with grant option;
+--     alter default privileges for user supabase_admin in schema cron grant all
+--         on tables to postgres with grant option;
+--     alter default privileges for user supabase_admin in schema cron grant all
+--         on functions to postgres with grant option;
+-- 
+--     grant all privileges on all tables in schema cron to postgres with grant option;
+--     revoke all on table cron.job from postgres;
+--     grant select on table cron.job to postgres with grant option;
+--   END IF;
+-- END;
+-- $$; -- Supabase-managed function, commented out
 
 
 --
 -- Name: FUNCTION grant_pg_cron_access(); Type: COMMENT; Schema: extensions; Owner: -
 --
 
-COMMENT ON FUNCTION extensions.grant_pg_cron_access() IS 'Grants access to pg_cron';
+-- COMMENT ON FUNCTION extensions.grant_pg_cron_access() IS 'Grants access to pg_cron'; -- Supabase-managed function
 
 
 --
 -- Name: grant_pg_graphql_access(); Type: FUNCTION; Schema: extensions; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION extensions.grant_pg_graphql_access() RETURNS event_trigger
-    LANGUAGE plpgsql
-    AS $_$
-DECLARE
-    func_is_graphql_resolve bool;
-BEGIN
-    func_is_graphql_resolve = (
-        SELECT n.proname = 'resolve'
-        FROM pg_event_trigger_ddl_commands() AS ev
-        LEFT JOIN pg_catalog.pg_proc AS n
-        ON ev.objid = n.oid
-    );
-
-    IF func_is_graphql_resolve
-    THEN
-        -- Update public wrapper to pass all arguments through to the pg_graphql resolve func
-        DROP FUNCTION IF EXISTS graphql_public.graphql;
-        create or replace function graphql_public.graphql(
-            "operationName" text default null,
-            query text default null,
-            variables jsonb default null,
-            extensions jsonb default null
-        )
-            returns jsonb
-            language sql
-        as $$
-            select graphql.resolve(
-                query := query,
-                variables := coalesce(variables, '{}'),
-                "operationName" := "operationName",
-                extensions := extensions
-            );
-        $$;
+-- CREATE FUNCTION extensions.grant_pg_graphql_access() RETURNS event_trigger -- Supabase-managed function, commented out
+--     LANGUAGE plpgsql
+--     AS $_$
+-- DECLARE
+--     func_is_graphql_resolve bool;
+-- BEGIN
+--     func_is_graphql_resolve = (
+--         SELECT n.proname = 'resolve'
+--         FROM pg_event_trigger_ddl_commands() AS ev
+--         LEFT JOIN pg_catalog.pg_proc AS n
+--         ON ev.objid = n.oid
+--     );
+-- 
+--     IF func_is_graphql_resolve
+--     THEN
+--         -- Update public wrapper to pass all arguments through to the pg_graphql resolve func
+--         DROP FUNCTION IF EXISTS graphql_public.graphql;
+--         create or replace function graphql_public.graphql(
+--             "operationName" text default null,
+--             query text default null,
+--             variables jsonb default null,
+--             extensions jsonb default null
+--         )
+--             returns jsonb
+--             language sql
+--         as $$
+--             select graphql.resolve(
+--                 query := query,
+--                 variables := coalesce(variables, '{}'),
+--                 "operationName" := "operationName",
+--                 extensions := extensions
+--             );
+--         $$; -- Supabase-managed function, commented out
 
         -- This hook executes when `graphql.resolve` is created. That is not necessarily the last
         -- function in the extension so we need to grant permissions on existing entities AND
@@ -200,136 +200,136 @@ $_$;
 -- Name: FUNCTION grant_pg_graphql_access(); Type: COMMENT; Schema: extensions; Owner: -
 --
 
-COMMENT ON FUNCTION extensions.grant_pg_graphql_access() IS 'Grants access to pg_graphql';
+-- COMMENT ON FUNCTION extensions.grant_pg_graphql_access() IS 'Grants access to pg_graphql'; -- Supabase-managed function
 
 
 --
 -- Name: grant_pg_net_access(); Type: FUNCTION; Schema: extensions; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION extensions.grant_pg_net_access() RETURNS event_trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
-  IF EXISTS (
-    SELECT 1
-    FROM pg_event_trigger_ddl_commands() AS ev
-    JOIN pg_extension AS ext
-    ON ev.objid = ext.oid
-    WHERE ext.extname = 'pg_net'
-  )
-  THEN
-    IF NOT EXISTS (
-      SELECT 1
-      FROM pg_roles
-      WHERE rolname = 'supabase_functions_admin'
-    )
-    THEN
-      CREATE USER supabase_functions_admin NOINHERIT CREATEROLE LOGIN NOREPLICATION;
-    END IF;
-
-    GRANT USAGE ON SCHEMA net TO supabase_functions_admin, postgres, anon, authenticated, service_role;
-
-    IF EXISTS (
-      SELECT FROM pg_extension
-      WHERE extname = 'pg_net'
-      -- all versions in use on existing projects as of 2025-02-20
-      -- version 0.12.0 onwards don't need these applied
-      AND extversion IN ('0.2', '0.6', '0.7', '0.7.1', '0.8', '0.10.0', '0.11.0')
-    ) THEN
-      ALTER function net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) SECURITY DEFINER;
-      ALTER function net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) SECURITY DEFINER;
-
-      ALTER function net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) SET search_path = net;
-      ALTER function net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) SET search_path = net;
-
-      REVOKE ALL ON FUNCTION net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) FROM PUBLIC;
-      REVOKE ALL ON FUNCTION net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) FROM PUBLIC;
-
-      GRANT EXECUTE ON FUNCTION net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) TO supabase_functions_admin, postgres, anon, authenticated, service_role;
-      GRANT EXECUTE ON FUNCTION net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) TO supabase_functions_admin, postgres, anon, authenticated, service_role;
-    END IF;
-  END IF;
-END;
-$$;
+-- CREATE FUNCTION extensions.grant_pg_net_access() RETURNS event_trigger -- Supabase-managed function, commented out
+--     LANGUAGE plpgsql
+--     AS $$
+-- BEGIN
+--   IF EXISTS (
+--     SELECT 1
+--     FROM pg_event_trigger_ddl_commands() AS ev
+--     JOIN pg_extension AS ext
+--     ON ev.objid = ext.oid
+--     WHERE ext.extname = 'pg_net'
+--   )
+--   THEN
+--     IF NOT EXISTS (
+--       SELECT 1
+--       FROM pg_roles
+--       WHERE rolname = 'supabase_functions_admin'
+--     )
+--     THEN
+--       CREATE USER supabase_functions_admin NOINHERIT CREATEROLE LOGIN NOREPLICATION;
+--     END IF;
+-- 
+--     GRANT USAGE ON SCHEMA net TO supabase_functions_admin, postgres, anon, authenticated, service_role;
+-- 
+--     IF EXISTS (
+--       SELECT FROM pg_extension
+--       WHERE extname = 'pg_net'
+--       -- all versions in use on existing projects as of 2025-02-20
+--       -- version 0.12.0 onwards don't need these applied
+--       AND extversion IN ('0.2', '0.6', '0.7', '0.7.1', '0.8', '0.10.0', '0.11.0')
+--     ) THEN
+--       ALTER function net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) SECURITY DEFINER;
+--       ALTER function net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) SECURITY DEFINER;
+-- 
+--       ALTER function net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) SET search_path = net;
+--       ALTER function net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) SET search_path = net;
+-- 
+--       REVOKE ALL ON FUNCTION net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) FROM PUBLIC;
+--       REVOKE ALL ON FUNCTION net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) FROM PUBLIC;
+-- 
+--       GRANT EXECUTE ON FUNCTION net.http_get(url text, params jsonb, headers jsonb, timeout_milliseconds integer) TO supabase_functions_admin, postgres, anon, authenticated, service_role;
+--       GRANT EXECUTE ON FUNCTION net.http_post(url text, body jsonb, params jsonb, headers jsonb, timeout_milliseconds integer) TO supabase_functions_admin, postgres, anon, authenticated, service_role;
+--     END IF;
+--   END IF;
+-- END;
+-- $$; -- Supabase-managed function, commented out
 
 
 --
 -- Name: FUNCTION grant_pg_net_access(); Type: COMMENT; Schema: extensions; Owner: -
 --
 
-COMMENT ON FUNCTION extensions.grant_pg_net_access() IS 'Grants access to pg_net';
+-- COMMENT ON FUNCTION extensions.grant_pg_net_access() IS 'Grants access to pg_net'; -- Supabase-managed function
 
 
 --
 -- Name: pgrst_ddl_watch(); Type: FUNCTION; Schema: extensions; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION extensions.pgrst_ddl_watch() RETURNS event_trigger
-    LANGUAGE plpgsql
-    AS $$
-DECLARE
-  cmd record;
-BEGIN
-  FOR cmd IN SELECT * FROM pg_event_trigger_ddl_commands()
-  LOOP
-    IF cmd.command_tag IN (
-      'CREATE SCHEMA', 'ALTER SCHEMA'
-    , 'CREATE TABLE', 'CREATE TABLE AS', 'SELECT INTO', 'ALTER TABLE'
-    , 'CREATE FOREIGN TABLE', 'ALTER FOREIGN TABLE'
-    , 'CREATE VIEW', 'ALTER VIEW'
-    , 'CREATE MATERIALIZED VIEW', 'ALTER MATERIALIZED VIEW'
-    , 'CREATE FUNCTION', 'ALTER FUNCTION'
-    , 'CREATE TRIGGER'
-    , 'CREATE TYPE', 'ALTER TYPE'
-    , 'CREATE RULE'
-    , 'COMMENT'
-    )
-    -- don't notify in case of CREATE TEMP table or other objects created on pg_temp
-    AND cmd.schema_name is distinct from 'pg_temp'
-    THEN
-      NOTIFY pgrst, 'reload schema';
-    END IF;
-  END LOOP;
-END; $$;
+-- CREATE FUNCTION extensions.pgrst_ddl_watch() RETURNS event_trigger -- Supabase-managed function, commented out
+--     LANGUAGE plpgsql
+--     AS $$
+-- DECLARE
+--   cmd record;
+-- BEGIN
+--   FOR cmd IN SELECT * FROM pg_event_trigger_ddl_commands()
+--   LOOP
+--     IF cmd.command_tag IN (
+--       'CREATE SCHEMA', 'ALTER SCHEMA'
+--     , 'CREATE TABLE', 'CREATE TABLE AS', 'SELECT INTO', 'ALTER TABLE'
+--     , 'CREATE FOREIGN TABLE', 'ALTER FOREIGN TABLE'
+--     , 'CREATE VIEW', 'ALTER VIEW'
+--     , 'CREATE MATERIALIZED VIEW', 'ALTER MATERIALIZED VIEW'
+--     , 'CREATE FUNCTION', 'ALTER FUNCTION'
+--     , 'CREATE TRIGGER'
+--     , 'CREATE TYPE', 'ALTER TYPE'
+--     , 'CREATE RULE'
+--     , 'COMMENT'
+--     )
+--     -- don't notify in case of CREATE TEMP table or other objects created on pg_temp
+--     AND cmd.schema_name is distinct from 'pg_temp'
+--     THEN
+--       NOTIFY pgrst, 'reload schema';
+--     END IF;
+--   END LOOP;
+-- END; $$; -- Supabase-managed function, commented out
 
 
 --
 -- Name: pgrst_drop_watch(); Type: FUNCTION; Schema: extensions; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION extensions.pgrst_drop_watch() RETURNS event_trigger
-    LANGUAGE plpgsql
-    AS $$
-DECLARE
-  obj record;
-BEGIN
-  FOR obj IN SELECT * FROM pg_event_trigger_dropped_objects()
-  LOOP
-    IF obj.object_type IN (
-      'schema'
-    , 'table'
-    , 'foreign table'
-    , 'view'
-    , 'materialized view'
-    , 'function'
-    , 'trigger'
-    , 'type'
-    , 'rule'
-    )
-    AND obj.is_temporary IS false -- no pg_temp objects
-    THEN
-      NOTIFY pgrst, 'reload schema';
-    END IF;
-  END LOOP;
-END; $$;
+-- CREATE FUNCTION extensions.pgrst_drop_watch() RETURNS event_trigger -- Supabase-managed function, commented out
+--     LANGUAGE plpgsql
+--     AS $$
+-- DECLARE
+--   obj record;
+-- BEGIN
+--   FOR obj IN SELECT * FROM pg_event_trigger_dropped_objects()
+--   LOOP
+--     IF obj.object_type IN (
+--       'schema'
+--     , 'table'
+--     , 'foreign table'
+--     , 'view'
+--     , 'materialized view'
+--     , 'function'
+--     , 'trigger'
+--     , 'type'
+--     , 'rule'
+--     )
+--     AND obj.is_temporary IS false -- no pg_temp objects
+--     THEN
+--       NOTIFY pgrst, 'reload schema';
+--     END IF;
+--   END LOOP;
+-- END; $$; -- Supabase-managed function, commented out
 
 
 --
 -- Name: set_graphql_placeholder(); Type: FUNCTION; Schema: extensions; Owner: -
 --
 
-CREATE OR REPLACE FUNCTION extensions.set_graphql_placeholder() RETURNS event_trigger
+CREATE OR REPLACE FUNCTION () RETURNS event_trigger
     LANGUAGE plpgsql
     AS $_$
     DECLARE
@@ -804,4 +804,3 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20251026174201'),
 ('20251026164854'),
 ('20251007183416');
-
